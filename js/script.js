9e14994e953ef966ce1577ba70f4ba922e0d6a09
stephanietsou <82925550+stephanietsou@users.noreply.github.com>
2021-11-10 15:38:36 -0800

    Modifying home page from homework 20
    took code from previous homework 20 and modifying

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// $(document).ready(function(){
+//     $("button").hover(function () {
+//         $(this).addClass("rotate");
+//     }, function () {
+//         $(this).removeClass("rotate");
+//     });
+//     $(".intro").hover(function(){
+//         $(this).addClass("green");
+//     }, function () {
+//         $(this).removeClass("green");
+//     })
+
+//     $(".inline-block").on( {
+//         mouseenter: function() {
+//             $(this).css({
+//                 "font-style": "bold"
+//             });
+//         }
+//     });
+
+
+
+
+
+
+
+
+    // $( "h1" ).clone().appendTo( document.body); 
+// $( window ).scroll(function() {
+//   $( ".profile" ).css( "display", "inline" ).fadeOut( "slow" );
+// });
+
+// }); //end of document.ready function
