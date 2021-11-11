128d85e048db03dbc265048a9d02a85742128a84
stephanietsou <82925550+stephanietsou@users.noreply.github.com>
2021-11-11 00:16:49 -0800

    adding about page and smooth scrolling
    smooth scroll using javascript to jump from top of homepage to "skills"

--- js/script.js
+++ js/script.js
@@ -1,22 +1,59 @@
-// $(document).ready(function(){
-//     $("button").hover(function () {
-//         $(this).addClass("rotate");
-//     }, function () {
-//         $(this).removeClass("rotate");
-//     });
-//     $(".intro").hover(function(){
-//         $(this).addClass("green");
-//     }, function () {
-//         $(this).removeClass("green");
-//     })
+$(document).ready(function(){
+    // $("button").hover(function () {
+    //     $(this).addClass("rotate");
+    // }, function () {
+    //     $(this).removeClass("rotate");
+    // });
+    // $(".intro").hover(function(){
+    //     $(this).addClass("green");
+    // }, function () {
+    //     $(this).removeClass("green");
+    // })
 
-//     $(".inline-block").on( {
-//         mouseenter: function() {
-//             $(this).css({
-//                 "font-style": "bold"
-//             });
-//         }
-//     });
+    // $(".inline-block").on( {
+    //     mouseenter: function() {
+    //         $(this).css({
+    //             "font-style": "bold"
+    //         });
+    //     }
+    // });
+
+    // Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
 
 
 
@@ -30,4 +67,4 @@
 //   $( ".profile" ).css( "display", "inline" ).fadeOut( "slow" );
 // });
 
-// }); //end of document.ready function
+}); //end of document.ready function
